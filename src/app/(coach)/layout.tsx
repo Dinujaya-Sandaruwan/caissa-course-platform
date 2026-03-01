f5ba69a8ecb72e0950a217c53c187732315565e5
Dinujaya Sandaruwan <88492493+Dinujaya-Sandaruwan@users.noreply.github.com>
2026-03-01 18:46:26 +0530

    Style: Adjust coach layout padding and remove max-width centering.

--- src/app/(coach)/layout.tsx
+++ src/app/(coach)/layout.tsx
@@ -100,9 +100,7 @@ export default async function CoachLayout({
 
         {/* Main Content */}
         <main className="flex-1 flex flex-col min-w-0 z-10 relative">
-          <div className="flex-1 p-4 sm:p-6 md:p-10 max-w-7xl mx-auto w-full">
-            {children}
-          </div>
+          <div className="flex-1 p-4 sm:p-6 md:p-8 w-full">{children}</div>
         </main>
       </div>
     </>
